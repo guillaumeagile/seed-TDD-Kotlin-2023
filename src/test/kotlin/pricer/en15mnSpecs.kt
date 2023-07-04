0abbbb0b668d91cc7966ea4a6c6cca1e254ef9af
guillaume <guillaume.saintetienne@marvin-bot.com>
2023-07-05 00:49:59 +0200

    little refacto

--- src/test/kotlin/pricer/en15mnSpecs.kt
+++ src/test/kotlin/pricer/en15mnSpecs.kt
@@ -7,23 +7,23 @@ import java.math.BigDecimal
 
 class en15mnSpecs : BehaviorSpec({
     given("un article") {
-        var article = Article()
+        val article = Article()
         `when`("acheté en quantité ") {
-            var article3 =  article.acheté(3)
+            val article3 =  article.acheté(3)
             then("qtté est ok") {
                 article3.quantité shouldBe 3
             }
         }
 
         `when`("acheté en quantité avec un prix de 2,5 ") {
-            var articleEn3QuiCoute25 =  article.acheté(3).auPrixUnitaireDe( BigDecimal( 2.5))
+            val articleEn3QuiCoute25 =  article.acheté(3).auPrixUnitaireDe( BigDecimal( 2.5))
             then("qtté est ok") {
                 articleEn3QuiCoute25.prixTotal shouldBeEqualComparingTo BigDecimal(7.5)
             }
         }
 
         `when`("acheté en quantité 2 avec un prix de 2,5 ") {
-            var articleEn3QuiCoute25 =  article.acheté(2).auPrixUnitaireDe( BigDecimal( 2.5))
+            val articleEn3QuiCoute25 =  article.acheté(2).auPrixUnitaireDe( BigDecimal( 2.5))
             then("prix est ok") {
                 articleEn3QuiCoute25.prixTotal shouldBeEqualComparingTo   BigDecimal(5.0)
             }
